de789e0567c0e42a5ed694ec36f21dde299eb60a
Luiz-Pastor <luigpacr5@gmail.com>
2025-01-20 16:45:53 +0100

    On the config file, added the links to the new cgis connections (binaries and html files). On code, deleted an useless comment of code

--- src/Methods/post_method.cpp
+++ src/Methods/post_method.cpp
@@ -100,47 +100,10 @@ CGIClient*	post_method( std::string path, Client& client, HTTPRequest* request)
 	std::string cgi_found = location.second->get_cgi( extension );
 
 	CGIClient *cgiclient = NULL;
-	if ( cgi_found.empty() ) {
-
-		// /* NOTE: 2.1. Case is not CGI, so its a file */
-		// struct stat file_data;
-		// std::pair<bool, bool> file_status = check_mode( path, &file_data, S_IWUSR );
-
-		// /* Path does not exist */
-		// if ( !file_status.first )
-		// 	return error_send( client, 404, location.second->get_error_page( 404 ), request );
-
-		// /* Path does not have enough permissions (write and execute for upload files) */
-		// if ( !file_status.second )
-		// 	return error_send( client, 403, location.second->get_error_page( 403 ), request );
-
-		// /* If path end is a file get that filename, otherwise get name from get_filename function */
-		// std::string dest_filename;
-		// if ( !S_ISDIR(file_data.st_mode) )
-		// 	dest_filename = path;
-		// else {
-		// 	if ( !(file_data.st_mode & S_IXUSR) )
-		// 		return error_send( client, 403, location.second->get_error_page( 403 ), request );
-
-		// 	std::string new_path = ( path[ path.size() - 1 ] == '/' ) ? path : path + "/";
-
-		// 	if ( request->get_filename().find_last_of('/') == std::string::npos )
-		// 		dest_filename = new_path + request->get_filename();
-		// 	else
-		// 		dest_filename = new_path + request->get_filename().substr( request->get_filename().find_last_of('/') + 1 );
-		// }
-		
-		// /* Change file path */
-		// request->move_body_file( dest_filename );
-
-		// /* Return response OK */
-		// error_send( client, 201, location.second->get_error_page( 201 ), request );
-
+	if ( cgi_found.empty() )
 		post_create( path, client, request, location.second );
-
-	} else {
+	else
 		cgiclient = post_cgi( path, cgi_found, client, request, location.second );
-	}
 
 	std::cout << "path [" << path << "]" << '\n';
 	return cgiclient;
