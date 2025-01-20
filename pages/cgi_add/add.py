7bdbad27468c1c0ad74efeef7e037a54f55dc5bd
Luiz-Pastor <luigpacr5@gmail.com>
2025-01-20 16:19:40 +0100

    Added a simple cgi (adder)

---
+++ pages/cgi_add/add.py
@@ -0,0 +1,13 @@
+from time import sleep
+import sys
+
+if __name__ == '__main__':
+	line = sys.stdin.read()
+
+	line = line.split("&")
+	n1 = int(line[0].split("=")[1])
+	n2 = int(line[1].split("=")[1])
+
+	print(f"Result: {n1 + n2}")
+
+	sys.exit(0)
